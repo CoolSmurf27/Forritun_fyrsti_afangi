53d5914f3a31c67b8cd0c949449144eb373d45fc
CoolSmurf27 <gardarsolvi@gmail.com>
2023-09-26 21:41:59 +0000

    Update 9_5.py

--- 9_5.py
+++ 9_5.py
@@ -1,28 +1,25 @@
+
 def main():
     name = get_name()
     age = get_age()
     print(f"Nice to meet you {name}. Congratulations on your {age} years.")
-
 def get_name() -> str:
-   while True:
-    name = input("What's your name? ")
-    if not name.replace(" ", "").isalpha() or not any(char.isalpha() for char in name):
-        print("Please enter a valid name.")
-    else:
-        return name
-
+    while True:
+        name = input("What's your name? ")
+        if not name.replace(" ", "").isalpha() or not any(char.isalpha() for char in name):
+            print("Please enter a valid name.")
+        else:
+            return name
 def get_age() -> str:
     while True:
+        age_str = input("How old are you? ")
         try:
-            age = int(input("How old are you? "))
-            
-            if age < 0 or age > 125:
-                print(f"You seriously expect me to believe you are {age} years old?")
+            age_int = int(age_str)
+            if age_int < 0 or age_int > 125:
+                print(f"You seriously expect me to believe you are {age_str} years old?")
             else:
-                return age
-        except ValueError: 
+                return age_str
+        except ValueError:
             print("Please enter an integer.")
-
-
 if __name__ == "__main__":
     main()
