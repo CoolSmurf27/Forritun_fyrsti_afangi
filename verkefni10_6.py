88f2338ea45731c2d01e79e1a993a43b70d8a932
CoolSmurf27 <gardarsolvi@gmail.com>
2023-09-28 20:03:09 +0000

    Update verkefni10_6.py

--- verkefni10_6.py
+++ verkefni10_6.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 
 
-def main():
-    def are_permutation_of_same_digits(m: str, n: str) -> bool:
-    
-        a = input()
-        b = input()
-        if are_permutation_of_same_digits(a, b):
-            print(f"The numbers {a} and {b} are permutations of each other.")
-        else:
-            print(f"The numbers {a} and {b} are not permutations of each other.")
-
 
 def are_permutation_of_same_digits(m: str, n: str) -> bool:
-    """Returns True if the given strings are permutations of each other, False otherwise."""
+    
+    return sorted(m) == sorted(n)
+        
+a = input()
+b = input()
+        
+if are_permutation_of_same_digits(a, b):
+    print(f"The numbers {a} and {b} are permutations of each other.")
+else:
+    print(f"The numbers {a} and {b} are not permutations of each other.")
 
-    # Implement this function.
-
-
-# Feel free to break it down into more functions.
-
-
-if __name__ == "__main__":
-    main()
