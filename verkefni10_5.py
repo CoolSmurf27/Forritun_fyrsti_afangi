40760cbfe702cbb72fc15ce6acce817f3c9dfb8e
CoolSmurf27 <gardarsolvi@gmail.com>
2023-09-28 19:54:42 +0000

    Update verkefni10_5.py

--- verkefni10_5.py
+++ verkefni10_5.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
-def count_problems(input_str):
-    chunks = input_str.split(';')
-    total_problems = 0
 
-    for chunk in chunks:
-        if '-' in chunk:
-            start, end = map(int, chunk.split('-'))
-            total_problems += (end - start + 1)
-        else:
-            total_problems += 1
+problems = input().split(';')
 
-    return total_problems
+total_problems = 0
+
+
+for part in problems:
+
+    if '-' in part:
+        start, end = map(int, part.split('-'))
+        total_problems += (end - start + 1)
+    else:
+        total_problems += 1
+
+print(total_problems)
+
 
 
 
